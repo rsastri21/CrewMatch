1cbd2a4216564c0e73c07be0f968b4e0493883fb
rsastri21 <rsastri21@gmail.com>
2022-12-16 16:10:07 -0600

    Initial commit.

---
+++ src/test/java/com/lux/crewmatch/CrewMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lux.crewmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
