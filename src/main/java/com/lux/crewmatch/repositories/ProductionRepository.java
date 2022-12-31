70cf5dc7525a48d1224b4d0dbbb5e4abeddd47a5
rsastri21 <rsastri21@gmail.com>
2022-12-30 19:11:05 -0600

    Implemented the matching service.

--- src/main/java/com/lux/crewmatch/repositories/ProductionRepository.java
+++ src/main/java/com/lux/crewmatch/repositories/ProductionRepository.java
@@ -3,6 +3,9 @@ package com.lux.crewmatch.repositories;
 import com.lux.crewmatch.entities.Production;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductionRepository extends CrudRepository<Production, Integer> {
+    Production findByName(String name);
 
 }
---
+++ src/main/java/com/lux/crewmatch/services/MatchService.java
@@ -0,0 +1,107 @@
+package com.lux.crewmatch.services;
+
+import com.lux.crewmatch.entities.Candidate;
+import com.lux.crewmatch.entities.Production;
+import com.lux.crewmatch.repositories.CandidateRepository;
+import com.lux.crewmatch.repositories.ProductionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.PriorityQueue;
+
+@Service
+public class MatchService {
+
+    // Repositories
+    @Autowired
+    CandidateRepository candidateRepository;
+
+    @Autowired
+    ProductionRepository productionRepository;
+
+    // Method to match candidates to productions
+    // Returns an http response stating how many candidates were matched
+    public ResponseEntity<String> match() {
+        // Only assigning unassigned candidates
+        List<Candidate> candidateList = candidateRepository.findByAssignedFalse();
+        int numCandidatesAssigned = 0;
+        int numProductions;
+
+        // Input processing:
+        // - If there are no candidates or no productions, the matching algorithm should not commence.
+        if (candidateList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("There are no candidates to match.");
+        }
+        if ((int) this.productionRepository.count() == 0) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("No productions have been created.");
+        }
+        numProductions = (int) this.productionRepository.count();
+
+        PriorityQueue<Candidate> orderedCandidates = new PriorityQueue<>(new CandidateComparator());
+        orderedCandidates.addAll(candidateList);
+
+        // Iterate through candidate in sorted order
+        while (!orderedCandidates.isEmpty()) {
+            Candidate candidate = orderedCandidates.poll();
+
+            if (candidate.getProdPriority()) {
+                // True when candidate prefers to be placed on desired production over role.
+                for (String production : candidate.getProductions()) {
+                    // Exit loop if candidate becomes assigned
+                    if (candidate.getAssigned()) {
+                        break;
+                    }
+
+                    Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
+                    if (productionOptional.isEmpty()) {
+                        continue;
+                    }
+
+                    // Try to place a candidate on a production with any roles.
+                    Production productionToTry = productionOptional.get();
+                    for (String role : candidate.getRoles()) {
+                        if (productionToTry.place(candidate, role)) {
+                            numCandidatesAssigned++;
+                            candidate.setAssigned(true);
+                            break;
+                        }
+                    }
+                }
+
+            } else {
+                // Branch where candidate prefers roles to productions.
+                for (String role : candidate.getRoles()) {
+                    // Exit loop if candidate becomes assigned
+                    if (candidate.getAssigned()) {
+                        break;
+                    }
+
+                    for (String production : candidate.getProductions()) {
+                        Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
+                        if (productionOptional.isEmpty()) {
+                            continue;
+                        }
+
+                        Production productionToTry = productionOptional.get();
+                        // Attempt to place with the given role
+                        if (productionToTry.place(candidate, role)) {
+                            numCandidatesAssigned++;
+                            candidate.setAssigned(true);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                numCandidatesAssigned + " have been placed on " + numProductions + " productions."
+        );
+
+    }
+
+}
