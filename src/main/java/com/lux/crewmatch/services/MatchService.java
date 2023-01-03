7728af0c3455f78da672baf632a6e84745225c0a
rsastri21 <rsastri21@gmail.com>
2023-01-02 20:52:21 -0600

    Implemented matchWithoutPreference() method and reduced redundancy. Currently untested.

--- src/main/java/com/lux/crewmatch/services/MatchService.java
+++ src/main/java/com/lux/crewmatch/services/MatchService.java
@@ -47,55 +47,11 @@ public class MatchService {
 
             if (candidate.getProdPriority()) {
                 // True when candidate prefers to be placed on desired production over role.
-                for (String production : new ArrayList<>(candidate.getProductions())) {
-                    // Exit loop if candidate becomes assigned
-                    if (candidate.getAssigned()) {
-                        break;
-                    }
-
-                    Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
-                    if (productionOptional.isEmpty()) {
-                        continue;
-                    }
-
-                    // Try to place a candidate on a production with any roles.
-                    Production productionToTry = productionOptional.get();
-                    for (String role : new ArrayList<>(candidate.getRoles())) {
-                        if (productionToTry.place(candidate, role)) {
-                            numCandidatesAssigned++;
-                            candidate.setAssigned(true);
-                            productionRepository.save(productionToTry);
-                            candidateRepository.save(candidate);
-                            break;
-                        }
-                    }
-                }
+                numCandidatesAssigned += assignToProductionWithProductionBias(candidate, new ArrayList<>(candidate.getRoles()), false);
 
             } else {
                 // Branch where candidate prefers roles to productions.
-                for (String role : new ArrayList<>(candidate.getRoles())) {
-                    // Exit loop if candidate becomes assigned
-                    if (candidate.getAssigned()) {
-                        break;
-                    }
-
-                    for (String production : new ArrayList<>(candidate.getProductions())) {
-                        Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
-                        if (productionOptional.isEmpty()) {
-                            continue;
-                        }
-
-                        Production productionToTry = productionOptional.get();
-                        // Attempt to place with the given role
-                        if (productionToTry.place(candidate, role)) {
-                            numCandidatesAssigned++;
-                            candidate.setAssigned(true);
-                            productionRepository.save(productionToTry);
-                            candidateRepository.save(candidate);
-                            break;
-                        }
-                    }
-                }
+                numCandidatesAssigned += assignToProductionWithRoleBias(candidate, new ArrayList<>(candidate.getProductions()), false);
             }
         }
 
@@ -103,6 +59,146 @@ public class MatchService {
                 numCandidatesAssigned + " have been placed on " + numProductions + " productions."
         );
 
+    }
+
+    // Method to match candidates without taking production/role preferences into consideration
+    // Returns an http response with how many candidates were matched.
+    public ResponseEntity<String> matchWithoutPreference() {
+        // Select unassigned candidates
+        List<Candidate> candidateList = this.candidateRepository.findByAssignedFalseAndActingInterestFalse();
+        int numCandidatesAssigned = 0;
+        int numProductions;
+
+        // Input processing
+        ResponseEntity<String> EXPECTATION_FAILED = getStringResponseEntity(candidateList);
+        if (EXPECTATION_FAILED != null) return EXPECTATION_FAILED;
+        numProductions = (int) this.productionRepository.count();
+
+        // Initialize ordered candidate list
+        PriorityQueue<Candidate> orderedCandidates = new PriorityQueue<>(new CandidateComparator());
+        orderedCandidates.addAll(candidateList);
+
+        while (!orderedCandidates.isEmpty()) {
+            Candidate candidate = orderedCandidates.poll();
+
+            // Based on production priority, assign candidate to any role in their top productions
+            // or any production with their top roles
+            if (candidate.getProdPriority()) {
+                // The roles to choose from will be determined by the roles in stored productions
+                numCandidatesAssigned += assignToProductionWithProductionBias(candidate, null, true);
+            } else {
+                // The productions to choose from will be determined by all available ones in the repository
+                numCandidatesAssigned += assignToProductionWithRoleBias(candidate, null, true);
+            }
+        }
+
+        // If there are still candidates to assign, do so without any preferences considered
+        // Last resort --> Nothing available that fits the candidates' choices
+        // Start by rechecking for candidates that are unmatched
+        candidateList = new ArrayList<>(this.candidateRepository.findByAssignedFalseAndActingInterestFalse());
+        orderedCandidates.addAll(candidateList);
+
+        while (!orderedCandidates.isEmpty()) {
+            Candidate candidate = orderedCandidates.poll();
+
+            // Obtain all productions
+            List<String> productions = obtainAllProductionNames();
+            for (String production : productions) {
+
+                Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
+                if (productionOptional.isEmpty()) {
+                    continue;
+                }
+                Production productionToTry = productionOptional.get();
+
+                // Try to place candidate in any role
+                for (String role : new ArrayList<>(productionToTry.getRoles())) {
+                    if (productionToTry.place(candidate, role)) {
+                        numCandidatesAssigned++;
+                        candidate.setAssigned(true);
+                        productionRepository.save(productionToTry);
+                        candidateRepository.save(candidate);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                numCandidatesAssigned + " have been placed on " + numProductions + " productions."
+        );
+
+    }
+
+    private Integer assignToProductionWithProductionBias(Candidate candidate, List<String> roles, boolean useProdRoles) {
+        int numCandidatesAssigned = 0;
+
+        for (String production : new ArrayList<>(candidate.getProductions())) {
+            // Exit loop if candidate becomes assigned
+            if (candidate.getAssigned()) {
+                break;
+            }
+
+            Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
+            if (productionOptional.isEmpty()) {
+                continue;
+            }
+
+            // Try to place a candidate on a production with any roles.
+            Production productionToTry = productionOptional.get();
+            List<String> rolesToUse = useProdRoles ? new ArrayList<>(productionToTry.getRoles()) : roles;
+            for (String role : rolesToUse) {
+                if (productionToTry.place(candidate, role)) {
+                    numCandidatesAssigned++;
+                    candidate.setAssigned(true);
+                    productionRepository.save(productionToTry);
+                    candidateRepository.save(candidate);
+                    break;
+                }
+            }
+        }
+
+        return numCandidatesAssigned;
+    }
+
+    private Integer assignToProductionWithRoleBias(Candidate candidate, List<String> productions, boolean useRepoProds) {
+        int numCandidatesAssigned = 0;
+
+        for (String role : new ArrayList<>(candidate.getRoles())) {
+            // Exit loop if candidate becomes assigned
+            if (candidate.getAssigned()) {
+                break;
+            }
+
+            List<String> prodsToUse;
+
+            // Obtain the names of all available productions
+            if (useRepoProds) {
+                prodsToUse = obtainAllProductionNames();
+            } else {
+                prodsToUse = productions;
+            }
+
+            for (String production : prodsToUse) {
+                Optional<Production> productionOptional = Optional.ofNullable(productionRepository.findByName(production));
+                if (productionOptional.isEmpty()) {
+                    continue;
+                }
+
+                Production productionToTry = productionOptional.get();
+                // Attempt to place with the given role
+                if (productionToTry.place(candidate, role)) {
+                    numCandidatesAssigned++;
+                    candidate.setAssigned(true);
+                    productionRepository.save(productionToTry);
+                    candidateRepository.save(candidate);
+                    break;
+                }
+            }
+        }
+
+        return numCandidatesAssigned;
     }
 
     // Helper method that determines if there are candidate and productions to be matched.
@@ -114,6 +210,16 @@ public class MatchService {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("No productions have been created.");
         }
         return null;
+    }
+
+    private List<String> obtainAllProductionNames() {
+        Iterable<Production> allProductions = productionRepository.findAll();
+        List<String> prodNames = new ArrayList<>();
+        for (Production prod : allProductions) {
+            prodNames.add(prod.getName());
+        }
+
+        return prodNames;
     }
 
 }
