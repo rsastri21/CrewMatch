f421a529060fe09d49ca324a0b8872c93b7186dc
rsastri21 <rsastri21@gmail.com>
2023-01-21 22:04:52 -0800

    Implemented User data model.

---
+++ src/main/java/com/lux/crewmatch/entities/User.java
@@ -0,0 +1,60 @@
+package com.lux.crewmatch.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    // Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "ROLE")
+    private String role;
+
+    public User() {
+
+    }
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
