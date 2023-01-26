9e87e67e6e009389e956a51b2a3f047d2d9c86a5
rsastri21 <rsastri21@gmail.com>
2023-01-26 11:53:55 -0800

    Re implemented swap method.

---
+++ src/main/java/com/lux/crewmatch/entities/SwapRequest.java
@@ -0,0 +1,36 @@
+package com.lux.crewmatch.entities;
+
+public class SwapRequest {
+
+    private String production1;
+    private String member1;
+    private String role1;
+
+    private String production2;
+    private String member2;
+    private String role2;
+
+    public String getProduction1() {
+        return production1;
+    }
+
+    public String getMember1() {
+        return member1;
+    }
+
+    public String getRole1() {
+        return role1;
+    }
+
+    public String getProduction2() {
+        return production2;
+    }
+
+    public String getMember2() {
+        return member2;
+    }
+
+    public String getRole2() {
+        return role2;
+    }
+}
