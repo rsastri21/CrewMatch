f2231cca89ad069ecd2444aa854f4606cde4e83e
rsastri21 <rsastri21@gmail.com>
2023-01-26 12:19:33 -0800

    Added SwapRequest entity model.

---
+++ src/main/java/com/lux/crewmatch/entities/SwapRequest.java
@@ -0,0 +1,13 @@
+package com.lux.crewmatch.entities;
+
+public class SwapRequest {
+
+    private String production1;
+    private String member1;
+    private String role1;
+
+    private String production2;
+    private String member2;
+    private String role2;
+
+}
